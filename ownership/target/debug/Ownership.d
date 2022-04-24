c5efcbd1154f71a97a169b8cac356e75a6b6dcc8
gluppler <guubsflow@gmail.com>
2022-04-24 23:01:57 +0800

    ownership commit

---
+++ ownership/target/debug/Ownership.d
@@ -0,0 +1,1 @@
+C:\Users\USER\projects\ownership\target\debug\ownership.exe: C:\Users\USER\projects\ownership\src\main.rs
---
+++ ownership/target/debug/deps/Ownership.d
@@ -0,0 +1,5 @@
+C:\Users\USER\projects\ownership\target\debug\deps\ownership.exe: src\main.rs
+
+C:\Users\USER\projects\ownership\target\debug\deps\ownership.d: src\main.rs
+
+src\main.rs:
